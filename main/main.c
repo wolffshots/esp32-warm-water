7af4ff5260c13da71f06978b375a5cdb22b25842
wolffshots <16850875+wolffshots@users.noreply.github.com>
2021-05-22 01:25:16 +0200

    added initial travis pages integration

--- main/main.c
+++ main/main.c
@@ -1,5 +1,34 @@
-/** 
- * esp-idf warm-water project
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 wolffshots
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * A note on the copyright line: https://www.copyright.gov/title17/92chap4.html#408
+ */
+
+/**
+ * @file main.c
+ * @author wolffshots
+ *
+ * esp-idf warm-water project. 
  * 
  * employs wifi, onewire and a relay to control the temperature
  * of a body of water for the purpose of film development
@@ -9,11 +38,6 @@
  * better process c41 colour film.
  */
 
-#include <stdio.h>     // esp packaged standard io library
-#include <stdbool.h>   // booleans
-#include "sdkconfig.h" // the generated configuration variables
-#include "led.h"       // led module
-
 /*
     ESP_LOGE - error (lowest)
     ESP_LOGW - warning
@@ -21,7 +45,10 @@
     ESP_LOGD - debug
     ESP_LOGV - verbose (highest)
 */
-
+#include <stdio.h>                   // esp packaged standard io library
+#include <stdbool.h>                 // booleans
+#include "sdkconfig.h"               // the generated configuration variables
+#include "led.h"                     // led module
 #ifdef CONFIG_ESP_ENABLE_WIFI        // check if wifi is enabled in sdkconfig
 #ifdef CONFIG_ESP_ENABLE_WIFI_STA    // check if station mode is enabled
 #include "wifi_sta.h"                // include station header if so
@@ -32,26 +59,32 @@
 #endif                               // CONFIG_ESP_ENABLE_WIFI
 #include "utility.h"                 // used for printing chip info
 #include "ds18b20_wrapper.h"         // for setting up and interfacing with the temp sensor via owb
-#include "timer.h"
+#include "timer.h"                   // wrappers for setting up high resolution timers
 
-int num_sensors = 0;
-esp_timer_handle_t periodic_temp_timer;
+int num_sensors = 0;                    ///< the number of sensors ds18b20 init has found (initially 0)
+esp_timer_handle_t periodic_temp_timer; ///< variable to control the timer associated with running the temperature polling
 
 static const char *TAG = "esp32-warm-water";
 
+/**
+ * polls the ds18b20 sensor for it's latest temperatures
+ * @param arg void pointer mainly because that's the fingerprint that the isr expects.
+ * a void pointer holds some address to an unknown type. read more at: 
+ * @link https://www.geeksforgeeks.org/void-pointer-c-cpp/
+ */
 static void poll_sensor_handler(void *arg)
 {
-    float results[sizeof(float) * num_sensors];    // create correctly sized float array for the sensor data
+    float results[sizeof(float) * num_sensors];    ///< create correctly sized float array for the sensor data
     ds18b20_wrapped_capture(results, num_sensors); // pass float array through to be captured to
     // check status and stuff here
-    for (int i = 0; i < num_sensors; ++i)
-    {
-        ESP_LOGI(TAG, "%d: %.3f\n", i, results[i]);
-    }
+    for (int i = 0; i < num_sensors; ++i)           // for each sensor
+    {                                               //
+        ESP_LOGI(TAG, "%d: %.3f\n", i, results[i]); // log result out
+    }                                               // this will probably be phased out once relay control is implemented
 }
-/*
-free resources and ensure safe shutdown of app
-*/
+/**
+ * free resources and ensure safe shutdown of app
+ */
 void app_deinit(void)
 {
     ds18b20_wrapped_deinit();
@@ -59,7 +92,9 @@ void app_deinit(void)
     // turn relay off
 }
 
-/** main method that is run by default (similar to arduino style setup) */
+/** 
+ * main method that is run by default (similar to arduino style setup) 
+ */
 void app_main(void)
 {
 #if defined(CONFIG_ESP_ENABLE_WIFI) && defined(CONFIG_ESP_ENABLE_WIFI_STA)    // check if station mode
